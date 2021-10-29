f5476f5668b1b16f29298078b4251b603a9e2e0f
FangYu199 <talentyupush@163.com>
2021-10-29 10:27:01 +0800

    Add files via upload

--- __ENV.js
+++ __ENV.js
@@ -1,1 +1,1 @@
-window.__ENV = {"REACT_APP_VOTE_URL":"https://tvote.sunder.finance/","REACT_APP_CHAIN_Id":"1"};
+window.__ENV = {"REACT_APP_VOTE_URL":"https://vote.sunder.finance/","REACT_APP_CHAIN_Id":"1"};
